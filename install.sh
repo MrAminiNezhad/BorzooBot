eda85def25df1e9005b9635df008f46298f5b710
Pouria amini <43442483+MrAminiNezhad@users.noreply.github.com>
2023-07-11 17:32:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 apt-get update
 apt-get install -y python3
 
-pip3 install python-telegram-bot==12.8.0
+apt install python3-pip
 
-pip3 install persiantools
+pip install python-telegram-bot==12.8.0
+
+pip install persiantools
 
 wget https://raw.githubusercontent.com/MrAminiNezhad/3x-ui-Sanaei-TelegramBot/main/main.py
