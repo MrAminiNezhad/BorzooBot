8af2d4cf84dec1e2a764d2102abe0c3c3ff89383
Pouria amini <43442483+MrAminiNezhad@users.noreply.github.com>
2023-07-14 17:07:10 +0330

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,7 @@ apt-get update
 wget https://raw.githubusercontent.com/MrAminiNezhad/3x-ui-Sanaei-TelegramBot/main/main.py
 
 wget https://raw.githubusercontent.com/MrAminiNezhad/3x-ui-Sanaei-TelegramBot/main/info.json
+
+pkill -f "main.py"
+
+nohup python3 main.py &
